2729a47317604337c9ae00e5c044d98b6b9751bf
Olle Kaiser <olle.kaiser@learnet.se>
2019-10-17 15:19:45 +0200

    New design, updated readme. New logo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-![](design/logo_140.png)
+![](design/banner.png)
+
+# User stories, https://US.YGSTR.COM
+Create user stories and share them with others.
+
+## Features
+* No server required, everything will work with static html
+* Create your own story templates and share them as well.
+* Share a short link (for what it's packing) that includes all your stories and custom templates
+* Use CTRL+Z to undo actions and typing.
+* For every change, the link is updated making sure you can't lose your stories if you accidentally closed the website (It's in your history)
+
+![](design/readme-screenshot.png)
--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 var auto_increment = 0;
 var stories = {};
 var last_link = "";
-var loading = true;
 
 // Animating images
-animate_logo("logo", "design/logo_small_xx.png", 4, 200);
+animate_logo("logo", "design/logo_small_black_xx.png", 3, 200);
 animate_logo("share-notation", "design/share_x.png", 2, 250);
 
 window.onload = () => {
     load_template_options();
     preload_story();
-    loading = true;
     if (Object.keys(stories).length == 0) add_story(undefined, undefined, true);
-    loading = false;
 };
 
 window.onpopstate = e => {
     preload_story();
+};
+
+
+String.prototype.pixelLength = function() {
+    var ruler = document.getElementById("ruler");
+    ruler.innerHTML = this;
+    return ruler.offsetWidth;
+};
+
+String.prototype.nthIndex = function(pat, n) {
+    n++;
+    var L = this.length,
+        i = -1;
+    while (n-- && i++ < L) {
+        i = this.indexOf(pat, i);
+        if (i < 0) break;
+    }
+    return i;
 };
 
 /**
@@ -55,22 +70,17 @@ function animate_logo(target, path, length, speed = 200) {
     return loop;
 }
 
-String.prototype.pixelLength = function() {
-    var ruler = document.getElementById("ruler");
-    ruler.innerHTML = this;
-    return ruler.offsetWidth;
-};
 
-String.prototype.nthIndex = function(pat, n) {
-    n++;
-    var L = this.length,
-        i = -1;
-    while (n-- && i++ < L) {
-        i = this.indexOf(pat, i);
-        if (i < 0) break;
-    }
-    return i;
-};
+
+var keys_down = []
+document.addEventListener('keydown', e => {
+    keys_down[e.keyCode] = true
+    if(keys_down[17] && keys_down[90]) history.back()
+})
+document.addEventListener('keyup', e => {
+    keys_down[e.keyCode] = false
+    
+})
 
 function preload_story() {
     loading = true;
@@ -78,8 +88,26 @@ function preload_story() {
     document.getElementById("stories").innerHTML = "";
 
     var search = location.search;
+
     if (search) {
-        var s = search.substr(search.indexOf("s=") + 2);
+        var start = search.indexOf("s=");
+        var stop = search.indexOf("&", start);
+
+        var s = search.substr(
+            search.indexOf("s=") + 2,
+            (stop != -1 ? stop : search.length) - start - 2
+        );
+
+        if (search.indexOf("t=") != -1) {
+            var custom_templates = search.substr(search.indexOf("t=") + 2);
+            custom_templates = decodeURIComponent(custom_templates);
+            custom_templates = atob(custom_templates);
+            custom_templates = JSON.parse(custom_templates);
+            for (var template of custom_templates) {
+                add_custom_template(template);
+            }
+        }
+
         s = decodeURIComponent(s);
         s = atob(s);
         s = JSON.parse(s);
@@ -91,19 +119,73 @@ function preload_story() {
     }
 }
 
+function add_custom_template(string) {
+    var key =
+        string
+            .substr(0, 10)
+            .split("{")
+            .join("")
+            .split("}")
+            .join("") + "...";
+    templates[key] = {
+        key: key,
+        text: string,
+        custom: true
+    };
+    load_template_options();
+}
+
+function user_add_story(el) {
+    var value = el.value;
+    el.value = null;
+    if (value == "CREATE_CUSTOM_STORY") {
+        var template = prompt(
+            "Enter your story template here. For every word you want to be editable surround it with curcly brackes. Exmaple: A word that is {editable}!"
+        );
+        if (template && template.length >= 10) {
+            var key = template.substr(0, 10);
+            templates[key] = {
+                key: key,
+                text: template,
+                custom: true
+            };
+            load_template_options();
+            update_link();
+        } else {
+            alert("Story is too short.");
+        }
+    } else {
+        add_story(value);
+    }
+    push_history();
+}
+
 function add_story(template = undefined, inputs = []) {
-    loading = true;
     var el = document.getElementById("stories");
     if (!template) template = document.getElementById("choose-template").value;
     var story = generate_story(templates[template], inputs);
     el.appendChild(story);
-    loading = false;
+}
+
+function update_link() {
+    document.getElementById("story-link").value = generate_link();
 }
 
 function generate_link() {
-    if (Object.keys(stories).length == 0) return location.origin;
-    var compressed = encodeURIComponent(btoa(JSON.stringify(stories)));
-    return location.origin + "?s=" + compressed;
+    var custom_templates = [];
+    for (var key in templates)
+        if (templates[key].custom) custom_templates.push(templates[key].text);
+    var c_templates =
+        custom_templates.length > 0
+            ? encodeURIComponent(btoa(JSON.stringify(custom_templates)))
+            : false;
+    var c_stories = encodeURIComponent(btoa(JSON.stringify(stories)));
+    return (
+        location.origin +
+        "?s=" +
+        c_stories +
+        (c_templates ? "&t=" + c_templates : "")
+    );
 }
 
 function update_copy_button(text = "Copy link", color = "rgb(243, 243, 243)") {
@@ -114,17 +196,19 @@ function update_copy_button(text = "Copy link", color = "rgb(243, 243, 243)") {
 
 function load_template_options() {
     var select = document.getElementById("choose-template");
+    select.innerHTML = `<option value=null>Add story</option>`;
     for (key in templates) {
-        select.innerHTML += `<option value='${key}'>${key}</option>`;
+        select.innerHTML += `<option value='${key}'>* ${key}</option>`;
     }
+    select.innerHTML += `<option value='CREATE_CUSTOM_STORY'>Create your own template</option>`;
 }
 
 function copy_link() {
-    update_copy_button("Copied!", "#82e868");
+    update_copy_button("Copied!", "#6bcc60");
     navigator.clipboard.writeText(generate_link());
 }
 
-function update_story(el, prevent_push = false) {
+function update_story(el, push = false) {
     var index = el.getAttribute("input-index");
     var story = stories[el.getAttribute("story-id")];
     var template = templates[story.key].text;
@@ -139,7 +223,7 @@ function update_story(el, prevent_push = false) {
     var link = generate_link();
     document.getElementById("story-link").value = link;
 
-    if (!prevent_push && last_link != link) {
+    if (last_link != link && push) {
         last_link = link;
         push_history();
     }
@@ -148,6 +232,7 @@ function update_story(el, prevent_push = false) {
 
 function push_history() {
     window.history.pushState(null, "", generate_link());
+    console.log("PUSHED");
 }
 
 function set_width(el) {
@@ -155,7 +240,11 @@ function set_width(el) {
     return el;
 }
 
-function generate_story(template = templates["Som en roll"], inputs = [], prevent_push = false) {
+function generate_story(
+    template = templates["Som en roll"],
+    inputs = [],
+    prevent_push = false
+) {
     //<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
     // Setup story element
     var story = document.createElement("div");
@@ -192,7 +281,7 @@ function generate_story(template = templates["Som en roll"], inputs = [], preven
             input_field.setAttribute("story-id", id);
             input_field.setAttribute("value", value);
             input_field.setAttribute("input-index", input_index);
-            input_field.setAttribute("oninput", "update_story(this)");
+            input_field.setAttribute("oninput", "update_story(this, true)");
             input_field.classList.add("story-input");
             input_field.type = "text";
 
